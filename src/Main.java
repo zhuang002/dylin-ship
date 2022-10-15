50c71b8e3f58cade7827503b8c2f8eaae5a4faf0
zhuang002 <zhuang001@gmail.com>
2022-10-15 19:41:19 -0400

    using array

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		char[] list = {'B','F','T','L','C'};
+		String line  = sc.nextLine();
+		for (int i=0;i<line.length();i++) {
+			char c = line.charAt(i);
+			for (int j=0;j<list.length;j++) {
+				char ch = list[j];
+				if (c==ch) {
+					list[j] = '*';
+				}
+			}
+		}
+		for (int i=0;i<list.length;i++) {
+			if (list[i]!='*') {
+				System.out.print(list[i]);
+			}
+		}
+	}
+
+}
